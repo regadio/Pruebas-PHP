150bc2837769f8b999b1aea414ab25212c45ba41
Nombre Apellido <tu.email@fpcoruna.afundacion.org>
2022-11-09 14:53:39 +0100

    arreglar

--- sistema_usuarios/processRegister.php
+++ sistema_usuarios/processRegister.php
@@ -7,6 +7,7 @@ if (isset($_POST['register'])) {
     $username = $_POST['nombre'];
     $email = $_POST['email'];
     $password = $_POST['contraseña'];  
+    $rol=0;
 
     $consulta = $db->prepare("select * from usuario WHERE gmail=:email");
     $consulta->bindParam("email",$email);
@@ -17,10 +18,11 @@ if (isset($_POST['register'])) {
     }
 
     if ($consulta->rowCount() == 0) {
-        $consulta = $db->prepare("INSERT INTO usuario(nombre,contrasena,gmail,rol) VALUES (:username,:contrasena,:email,'0')");
+        $consulta = $db->prepare("INSERT INTO usuario(nombre,contrasena,gmail,rol) VALUES (:username,:contrasena,:email,:rol)");
         $consulta->bindParam("username", $username);
         $consulta->bindParam("contrasena", $password);
         $consulta->bindParam("email", $email);
+        $consulta->bindParam("rol", $rol);
         $result = $consulta->execute();
  
         if ($result) {
